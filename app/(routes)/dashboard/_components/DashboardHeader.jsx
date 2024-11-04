abc8069dc02e90c4ceb6f733b7def19eca0d6057
Vineet Kumar <ak6116007@gmail.com>
2024-11-04 19:59:13 +0530

    removed Search bar

--- app/(routes)/dashboard/_components/DashboardHeader.jsx
+++ app/(routes)/dashboard/_components/DashboardHeader.jsx
@@ -1,17 +1,15 @@
-import { UserButton } from '@clerk/nextjs'
-import React from 'react'
+import { UserButton } from "@clerk/nextjs";
+import React from "react";
 
 function DashboardHeader() {
   return (
-    <div className='p-5 border-b shadow-sm flex justify-between'>
-        <div>
-            search Bar
-        </div>
-        <div>
-            <UserButton/>
-        </div>
+    <div className="p-5 border-b shadow-sm flex justify-between">
+      <div></div>
+      <div>
+        <UserButton />
+      </div>
     </div>
-  )
+  );
 }
 
-export default DashboardHeader
+export default DashboardHeader;
